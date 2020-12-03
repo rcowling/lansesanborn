a0db5e216ef3b05fa945b56800e00a547e419180
Bob Cowling <rcowling@wisc.edu>
2020-12-03 14:15:47 -0500

    update z-index

--- bootstrap.min.45389b0b.js
+++ bootstrap.min.45389b0b.js
@@ -13865,7 +13865,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64152" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50259" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
